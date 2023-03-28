28a0b9a8d6da444c481c0311a6216c7c7dea5d1c
Guillaume Lamourec <parkerpool@live.fr>
2023-03-28 11:57:26 +0200

    Revert "used JS to make pokemoncard component display change depending on the index of an array of pokemons"
    This reverts commit 8c194f52a56cd4169b842856ddc7d0a8e578ed39.

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,20 +1,8 @@
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
-
 function PokemonCard() {
-    const pokemon = pokemonList[0]
     return (
     <figure>
-        {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon.name} /> : <p>???</p>}
-        <figcaption>{pokemon.name}</figcaption>
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="bulbasaur" />
+        <figcaption>Bulbasaur</figcaption>
     </figure>
     );
 }
