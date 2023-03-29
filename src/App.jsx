d9da9bee30424b0bc505dd386dca350f471a52ba
Guillaume Lamourec <parkerpool@live.fr>
2023-03-29 10:35:18 +0200

    managed to get the props working

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import "./App.css"
 import PokemonCard from "./components/PokemonCard"
 
 function App() {
+  const pokemonList = [
+    {
+      name: "Bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "Mew",
+    },
+  ];
+  
   return (
     <div>
-      <PokemonCard />
+      <PokemonCard pokemon={pokemonList[0]}/>
     </div>
   );
 }
